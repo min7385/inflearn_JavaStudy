926398a53472c2e05e3afa4d604a922b7f0bbe08
min <kang1234499@gmail.com>
2025-02-07 19:10:35 +0900

    'feature/ovrriding'

---
+++ java-basic/src2/poly/basic/overriding/OverridingMain.java
@@ -0,0 +1,26 @@
+package poly.basic.overriding;
+
+public class OverridingMain {
+
+	public static void main(String[] args) {
+		// 자식 변수가 자식 인스턴스 참조
+		Child child = new Child();
+		System.out.println("Child -> Child");
+		System.out.println("value = " + child.value);
+		child.method();
+		
+		// 부모 변수가 부모 인스턴스 참조
+		Parent parent = new Parent();
+		System.out.println("Parent -> Parent");
+		System.out.println("value = " + parent.value);
+		parent.method();
+		
+		// 부모 변수가 자식 인스턴스를 참조(다형적 참조)
+		Parent poly = new Child();
+		System.out.println("Parent -> Child");
+		System.out.println("value = " + poly.value);	// 변수는 오버라이딩x
+		poly.method();	// 메소드는 오버라이딩o
+		// 오버라이딩된 메서드가 항상 우선권을 갖는다!!
+	}
+
+}
